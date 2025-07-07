75173fbddd3f9751aeaf32d3846c2e3f5397ed49
Thomas Ogolla <129851445+tomogolla@users.noreply.github.com>
2025-07-07 22:42:48 +0300

    Create settings.py

---
+++ alx_travel_app/alx_travel_app/settings.py
@@ -0,0 +1,9 @@
+import environ
+import os
+
+env = environ.Env(DEBUG=(bool, False))
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+SECRET_KEY = env('SECRET_KEY')
+DEBUG = env('DEBUG')
+ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(',')
